5b7e5b5589174babcbed6e05a03eaaea135c54ca
Andrew Kane <andrew@chartkick.com>
2020-10-10 00:26:39 -0700

    Updated tests [skip ci]

--- test/dmr_test.rb
+++ test/dmr_test.rb
@@ -5,10 +5,10 @@ class DMRTest < Minitest::Test
     model = Tomoto::DMR.new
     assert_kind_of Tomoto::LDA, model
     assert_in_delta 0.1, model.alpha
-    assert_in_delta 0.01, model.eta
-    assert_in_delta 1, model.sigma
     assert_in_delta 1e-10, model.alpha_epsilon
+    assert_in_delta 0.01, model.eta
     assert_equal 0, model.f
+    assert_in_delta 1, model.sigma
 
     model.add_doc(["new", "document"], metadata: "hello")
   end
