4e5ba4c356cbe11a653be85ef0fb155c7d2dfe49
Andrew Kane <andrew@chartkick.com>
2020-10-10 00:51:04 -0700

    Fixed warnings [skip ci]

--- test/hlda_test.rb
+++ test/hlda_test.rb
@@ -8,11 +8,11 @@ class HLDATest < Minitest::Test
 
     assert_equal [], model.children_topics(0)
     assert_equal 2, model.depth
-    assert_equal -1, model.level(0)
+    assert_equal(-1, model.level(0))
     refute model.live_topic?(0)
     assert_in_delta 0.1, model.gamma
     assert_equal 0, model.live_k
     assert_equal 0, model.num_docs_of_topic(0)
-    assert_equal -1, model.parent_topic(0)
+    assert_equal(-1, model.parent_topic(0))
   end
 end
