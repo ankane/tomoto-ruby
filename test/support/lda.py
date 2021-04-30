8e4cc2722339f370e5eb7552914bbe2bd71695aa
Andrew Kane <andrew@ankane.org>
2021-04-30 01:46:42 -0700

    Updated LDA script [skip ci]

--- test/support/lda.py
+++ test/support/lda.py
@@ -1,5 +1,23 @@
 import tomotopy as tp
 
+model = tp.LDAModel()
+print(model.alpha)
+print(model.eta)
+print(model.global_step)
+print(model.optim_interval)
+print(model.perplexity)
+print(model.tw)
+
+model.add_doc(["new", "document"])
+model.burn_in = 100
+print(model.burn_in)
+model.train(0)
+print(model.num_words)
+print(model.num_vocabs)
+print(model.vocabs)
+
+print(model.get_topic_words(0))
+
 model = tp.LDAModel(k=2, seed=42)
 model.add_doc(["this", "is", "a", "test"])
 model.add_doc(["another", "document"])
