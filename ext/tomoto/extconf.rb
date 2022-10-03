d470731a09c89244488e39e5919cd3ae49239bed
Andrew Kane <andrew@ankane.org>
2022-10-02 23:56:01 -0700

    Set -mavx512f

--- ext/tomoto/extconf.rb
+++ ext/tomoto/extconf.rb
@@ -2,7 +2,7 @@ require "mkmf-rice"
 
 $CXXFLAGS += " -std=c++17 $(optflags) -DEIGEN_MPL2_ONLY"
 
-# $CXXFLAGS += " -march=native"
+$CXXFLAGS += " -mavx512f"
 
 apple_clang = RbConfig::CONFIG["CC_VERSION_MESSAGE"] =~ /apple clang/i
 
