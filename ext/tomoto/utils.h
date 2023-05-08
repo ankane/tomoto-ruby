d31104a850d1da06925df64019837fe8f931182b
Andrew Kane <andrew@ankane.org>
2023-05-07 21:23:22 -0700

    Removed conversion [skip ci]

--- ext/tomoto/utils.h
+++ ext/tomoto/utils.h
@@ -6,22 +6,22 @@
 using Rice::Array;
 using Rice::Object;
 
-namespace Rice::detail
-{
-  template<typename T>
-  class To_Ruby<std::vector<T>>
-  {
-  public:
-    VALUE convert(std::vector<T> const & x)
-    {
-      auto a = rb_ary_new2(x.size());
-      for (const auto& v : x) {
-        detail::protect(rb_ary_push, a, To_Ruby<T>().convert(v));
-      }
-      return a;
-    }
-  };
-}
+// namespace Rice::detail
+// {
+//   template<typename T>
+//   class To_Ruby<std::vector<T>>
+//   {
+//   public:
+//     VALUE convert(std::vector<T> const & x)
+//     {
+//       auto a = rb_ary_new2(x.size());
+//       for (const auto& v : x) {
+//         detail::protect(rb_ary_push, a, To_Ruby<T>().convert(v));
+//       }
+//       return a;
+//     }
+//   };
+// }
 
 inline tomoto::RawDoc buildDoc(std::vector<std::string>& words) {
   tomoto::RawDoc doc;
